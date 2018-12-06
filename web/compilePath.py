52f9378e8fb1885d35e782d291111059bf125497
DuanYU <chinazzbcn@gmail.com>
2018-12-06 14:06:25 +0800

    add suse install tomcat

---
+++ web/compilePath.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#python: 2.7.x
+#organization: China Poka
+#Author: Duan Yu
+#mail:chinazzbcn@gmail.com or cn-duanyu@foxmail.com
+#Date: 2018-10-25
+#version: 0.0.2
+
+import py_compile
+import os
+
+compilePathFile = raw_input("请输入需要转换文件的绝对路径:")
+
+if not os.path.exists(compilePathFile):
+    print("此路径不存在，请检查后再尝试!!!!!!!")
+    os._exit(1)
+py_compile.compile(compilePathFile)
---
+++ web/test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#python: 2.7.x
+#organization: China Poka
+#Author: Duan Yu
+#mail:chinazzbcn@gmail.com or cn-duanyu@foxmail.com
+#Date: 2018-10-25
+#version: 0.0.2
+
+def replace(file_path, old_str, new_str):
+    try:
+        f = open(file_path,'r+')
+        all_lines = f.readlines()
+        f.seek(0)
+        f.truncate()
+        for line in all_lines:
+            line = line.replace(old_str, new_str)
+            f.write(line)
+        f.close()
+    except Exception,e:
+        print e
+if __name__ == "__main__":
+    replace("E:\\test\\test.txt","16GB","32GB")
