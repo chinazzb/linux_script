63512f29efeec8e5b996f9a0f1d211b4bb8e36e6
Duan Yu <chinazzbcn@gmail.com>
2019-03-19 17:11:25 +0800

    add test dir

---
+++ test/confTest.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#python: 2.7.x
+#organization: China Poka
+#Author: Duan Yu
+#mail:chinazzbcn@gmail.com or cn-duanyu@foxmail.com
+#Date: 2019/3/19
+
+import os
+import configparser
+
+cf = configparser.ConfigParser()
+
+cf.read("./test.ini",encoding="utf-8-sig")
+# #获得所有大项
+# secs = cf.sections()
+# print(secs,type(secs))
+# #获得db的参数
+# opts = cf.options("db")
+# print(opts)
+# #获得db的参数的值
+# kvs = cf.items("db")
+# print(kvs,type(kvs))
+nginx_processes = cf.get("nginx","proxy_tomcat")
+print(nginx_processes)
+
+
