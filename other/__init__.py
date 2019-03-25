f61f030daf3a7106d99a63dda5e4af8cbd3e3441
duany <chinazzbcn@gmail.cpm>
2019-03-25 20:05:55 +0800

    move a lot file

---
+++ other/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#python: 2.7.x
+#organization: China Poka
+#Author: Duan Yu
+#mail:chinazzbcn@gmail.com or cn-duanyu@foxmail.com
+#Date: 2019/3/25
+
+
+if __name__ == '__main__':
